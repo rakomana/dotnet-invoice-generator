e8999ce98303d915a6c4e87633d146d6998929ac
Prince Rakomana <Princer@clc.co.za>
2023-09-06 13:40:38 +0200

    add new fields for authentication

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,8 @@ namespace learnApi.Models
     public class User
     {
         public int Id {get; set;}
+        public string? UserName {get; set;}
+        public string? Password {get; set;}
         public string? FirstName {get; set;}
         public string? LastName {get; set;}
         public Quotation? Quotation {get; set;} 
